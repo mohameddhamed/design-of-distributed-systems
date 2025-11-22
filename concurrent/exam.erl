35fc9f886f25aa6db0a19d2a5262e0155839ee20
mohameddhamed <mohamedus.1970.hm@gmail.com>
2025-11-22 19:48:37 +0100

    conc/test1

--- concurrent/exam.erl
+++ concurrent/exam.erl
@@ -87,7 +87,7 @@ ordered_doubler(L) ->
 
 % applyAllPar(FS :: list(function()), LS :: list()) -> Result :: list()
 
-apply_and_send(Fct, H, Pid) -> Pid ! {self(), apply(Fct, [H])}.
+apply_and_send(Fct, Arg, Pid) -> Pid ! {self(), apply(Fct, [Arg])}.
 
 create_pid_list(Fct, [H | T]) ->
     [spawn(exam, apply_and_send, [Fct, H, self()]) | create_pid_list(Fct, T)];
@@ -172,3 +172,62 @@ loop(N, BestValue) ->
         {ok, _} -> loop(N - 1, BestValue);
         {error, _} -> loop(N - 1, BestValue)
     end.
+
+% Task 2: Speculative evaluation (15 points)
+
+% We have a list of functions and a list of values (which might be of different sizes).
+% We want to evaluate all the functions in parallel as follows,
+% the first function should be evaluated on the first data, the second on the second, etc.
+
+% Your task is to start all the processes at the same time and wait for the quickest response.
+% The result of the evaluation must be the quickest result.
+
+% If your solution handles the possible errors and waits for all the processes to finish
+% (hint: maintain a list), then returns the quickest result, your solution is worth 15 points.
+
+% speculativeEval(FS :: list(function()), LS :: list()) -> Result
+
+try_apply_send(Fun, Data, Pid) ->
+    try
+        Pid ! {self(), apply(Fun, [Data])}
+    catch
+        _:_ -> Pid ! {self(), no_proper_result}
+    end.
+
+create_pids([Fun | FS], [Data | LS]) ->
+    [spawn(exam, try_apply_send, [Fun, Data, self()]) | create_pids(FS, LS)];
+create_pids(_, []) ->
+    [];
+create_pids([], _) ->
+    [].
+
+receive_pids([_ | T], Best) ->
+    receive
+        {_, no_proper_result} -> receive_pids(T, Best);
+        {_, Value} when Best == no_proper_result -> receive_pids(T, Value);
+        {_, _} -> receive_pids(T, Best)
+    end;
+receive_pids([], Best) ->
+    Best.
+
+speculativeEval(Fs, Ls) ->
+    receive_pids(create_pids(Fs, Ls), no_proper_result).
+
+% For testing, add the following definition to your module, and be sure you export it.
+
+fib(0) -> 0;
+fib(1) -> 1;
+fib(N) -> fib(N - 1) + fib(N - 2).
+
+% Test cases
+
+% exam:speculativeEval([], []) == no_proper_result.
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], []) == no_proper_result.
+% exam:speculativeEval([], [10,20,21,22]) == no_proper_result.
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [10,20,21,22]) == 55. %% very probably
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [20,10,21,22]) == 55. %% very probably
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [22,24,20,10,21,22]) == 55.
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [apple, pear, 22, plum, foo, bar]) == 17711.
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [apple, pear, 22, plum, foo, bar]) == 17711.
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [apple, pear, 22, plum, 23, bar]) == 17711.
+% exam:speculativeEval([fun exam:fib/1, fun exam:fib/1,fun exam:fib/1, fun exam:fib/1], [apple, pear, plum, orange, foo, bar]) == no_proper_result.
